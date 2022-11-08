9d0e6f1d773d667f244af541b946c957a7891b54
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-11-08 16:09:42 +0100

    fixed condition to ensure that the array contains all emails

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,22 +3,26 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            emails: null,
+            max: 10,
+            emails: [],
             tempArr: []
         }
     },
+    methods: {
+
+    },
     mounted() {
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < this.max; i++) {
             axios
                 .get("https://flynn.boolean.careers/exercises/api/random/mail")
                 .then(response => {
                     // console.log(response.data.response);
                     this.tempArr.push(response.data.response);
+                    if (this.tempArr.length === this.max) {
+                        this.emails = this.tempArr;
+                    }
                 })
-        }
-        // console.log(this.tempArr);
 
-        this.emails = this.tempArr;
-        // console.log(this.emails);
+        }
     }
 }).mount('#app');
