9e4464ce0128fcba5c7fab5454d41fea3b00abde
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-11-08 15:07:39 +0100

    generated 10 email via axios

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,18 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: "Test"
+            emails: []
         }
+    },
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then(response => {
+                    // console.log(response.data.response);
+                    this.emails.push(response.data.response)
+                })
+        }
+        // console.log(this.emails);
     }
 }).mount('#app');
